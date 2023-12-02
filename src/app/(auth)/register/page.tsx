cd1fadd27b318153fcec0aafbd2891ca6d4c1670
Claudio Lins <claudio.lins@me.com>
2023-12-02 22:03:31 +0000

    feat: form and registe page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,4 +1,7 @@
+import { FormRegister } from "@/components/auth/form-register"
+import { Separator } from "@/components/ui/separator"
 import { auth } from "@/lib/auth"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 export default async function RegisterPage() {
@@ -6,8 +9,23 @@ export default async function RegisterPage() {
 
   if (session) redirect("/")
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="w-full flex items-center justify-center bg-zinc-950 px-2 min-h-screen py-6">
+      <div className="w-full max-w-sm rounded-lg bg-white px-2 py-6 flex items-center justify-center h-fit flex-col space-y-6">
+        <header className="flex flex-col items-center justify-center space-y-6">
+          <h1 className="text-2xl font-bold">Olá, crie sua conta.</h1>
+          <p className="text-center w-ful px-4">
+            Enter your email below to create your account.
+          </p>
+        </header>
+        <FormRegister />
+        <Separator className="w-full" />
+        <div className="flex items-center justify-center space-x-2">
+          <p className="text-sm">Já possuo cadastra?</p>
+          <Link href="/signin" className="text-sm text-blue-500">
+            Entrar
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/auth/form-register.tsx
@@ -0,0 +1,119 @@
+"use client"
+
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { toast } from "../ui/use-toast"
+
+interface UserLoginFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface IUserProps {
+  name: string
+  email: string
+  password: string
+}
+
+export function FormRegister({ className, ...props }: UserLoginFormProps) {
+  const router = useRouter()
+  const [data, setData] = useState<IUserProps>({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setIsLoading(true)
+
+    const request = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+
+    const response = await request.json()
+
+    if (!request.ok) {
+      toast({
+        title: "Erro ao criar usuário",
+        description: response.message,
+      })
+    } else {
+      router.push("/auth/login")
+    }
+
+    setData({
+      name: "",
+      email: "",
+      password: "",
+    })
+    setIsLoading(false)
+  }
+
+  function handleChance(e: React.ChangeEvent<HTMLInputElement>) {
+    setData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value }
+    })
+  }
+  return (
+    <form onSubmit={onSubmit} className="w-full flex flex-col space-y-4 px-6">
+      <div className="grid gap-2">
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="name">
+            Nome
+          </Label>
+          <Input
+            id="Name"
+            type="text"
+            placeholder="Nome completo"
+            disabled={isLoading}
+            name="name"
+            value={data.name}
+            onChange={handleChance}
+          />
+        </div>
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="email">
+            Email
+          </Label>
+          <Input
+            id="Email"
+            type="email"
+            placeholder="Email"
+            autoCapitalize="none"
+            autoCorrect="off"
+            disabled={isLoading}
+            name="email"
+            value={data.email}
+            onChange={handleChance}
+          />
+        </div>
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="password">
+            Password
+          </Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            autoCorrect="off"
+            disabled={isLoading}
+            name="password"
+            value={data.password}
+            onChange={handleChance}
+          />
+        </div>
+        <Button disabled={isLoading}>
+          {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          Registrar
+        </Button>
+      </div>
+    </form>
+  )
+}
